4fb00bce0919a73541473b619040d3356d6776af
unknown <abumohammadnayemkhan@gmail.com>
2023-08-12 21:16:13 +0600

    learning Javascript

---
+++ tpreact/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import { Home } from './pages/Home';
+import About from './pages/About';
+function App() {
+  return (
+    <div className="App">
+      <Home></Home>
+      <About></About>
+    </div>
+  );
+}
+
+export default App;
---
+++ tpreact/src/pages/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { About } from './About';
+
+export const Home = () => {
+  return (
+    <div>
+        <h1>Im from home</h1>
+    </div>
+  )
+}
